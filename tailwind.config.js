95efd221e12309d08dfda7ac1dac7943d150c53b
faria-r <fariacse11@gmail.com>
2023-08-13 12:39:42 +0600

    Footer added in main layout

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+   content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+    colors:{
+      "black":"#020617",
+      "white":"#FFFFFF",
+      "darkNavy":"#111827",
+      "black":"#020617",
+      "black":"#020617",
+      "black":"#020617",
+      "black":"#020617",
+      "black":"#020617",
+      "black":"#020617",
+      "black":"#020617",
+      "black":"#020617",
+    }
+  },
+  plugins: [require("daisyui")],
+}
+
