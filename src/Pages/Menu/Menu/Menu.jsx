6953a4601d34f9b58a271469875e12e58744ada7
faria-r <fariacse11@gmail.com>
2023-08-18 00:08:25 +0600

    Dynamic Component created for different Menu Category

--- src/Pages/Menu/Menu/Menu.jsx
+++ src/Pages/Menu/Menu/Menu.jsx
@@ -1,21 +1,52 @@
-import React from 'react';
-import { useTitle } from '../../../Hooks/useTitle/useTitle';
-import Cover from '../../Shared/Cover/Cover';
-import menuImg from '../../../assets/menu/banner3.jpg'
-import { useMenu } from '../../../Hooks/useMenu/useMenu';
+import React from "react";
+import { useTitle } from "../../../Hooks/useTitle/useTitle";
+import Cover from "../../Shared/Cover/Cover";
+import menuImg from "../../../assets/menu/banner3.jpg";
+import { useMenu } from "../../../Hooks/useMenu/useMenu";
+import SectionTitle from "../../../Components/SectionTitle/SectionTitle";
+import MenuCategory from "../MenuCategory/MenuCategory";
+import dessertImg from "../../../assets/menu/dessert-bg.jpeg";
 
 const Menu = () => {
-useTitle('Rio | Menu');
-const [menu] = useMenu('menu.json');
-console.log(menu)
-    return (
-        <div >
-            <div className='mb-16'>
-            <Cover  bgimg={menuImg} TitleName={"Our Menu"} details={'Would you like to try a dish?'}></Cover>
-            </div>
-           
-        </div>
-    );
+  useTitle("Rio | Menu");
+  const [menu] = useMenu("menu.json");
+
+  const desserts = menu.filter((item) => item.category === "dessert");
+  const dessertMenu = desserts.find((Item) => Item.category === "dessert");
+  const dessertDetails = dessertMenu.recipe;
+  const pizzas = menu.filter((item) => item.category === "pizza");
+  const salads = menu.filter((item) => item.category === "salad");
+  const soups = menu.filter((item) => item.category === "soup");
+  const offer = menu.filter((item) => item.category === "offered");
+  return (
+    <div>
+      <div className="mb-16">
+        {/* main cover */}
+        <Cover
+          bgimg={menuImg}
+          TitleName={"Our Menu"}
+          details={"Would you like to try a dish?"}
+        ></Cover>
+      </div>
+      <div>
+        {/* Todays Offer  */}
+        <SectionTitle
+          heading={`TODAY'S OFFER`}
+          subHeading={`Don't miss`}
+        ></SectionTitle>
+        <MenuCategory items={offer}></MenuCategory>
+      </div>
+      {/* Desserts  */}
+      <div>
+        <MenuCategory
+          TitleName={"Desserts"}
+          items={desserts}
+          details={dessertDetails}
+          bgimg={dessertImg}
+        ></MenuCategory>
+      </div>
+    </div>
+  );
 };
 
 export default Menu;
---
+++ src/Pages/Menu/MenuCategory/MenuCategory.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import MenuItem from '../../Shared/MenuItem/MenuItem';
+import Cover from '../../Shared/Cover/Cover';
+
+const MenuCategory = ({items,TitleName,details,bgimg}) => {
+    return (
+        <div>
+            {
+                TitleName && details && bgimg &&  <Cover TitleName={TitleName} details={details} bgimg={bgimg}></Cover>
+            }
+          
+            <div className="grid md:grid-cols-2 gap-4 p-4">
+        {items.map((item) => (
+            <MenuItem key={item._id} item={item}></MenuItem>
+          ))}
+      </div>
+        </div>
+    );
+};
+
+export default MenuCategory;
