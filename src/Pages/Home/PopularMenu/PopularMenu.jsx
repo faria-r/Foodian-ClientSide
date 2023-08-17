7b30263fd144b0f2c699e0a30115bc26e1cdf84a
faria-r <fariacse11@gmail.com>
2023-08-17 20:41:29 +0600

    data filtered from custom hook

--- src/Pages/Home/PopularMenu/PopularMenu.jsx
+++ src/Pages/Home/PopularMenu/PopularMenu.jsx
@@ -1,32 +1,33 @@
-import React, { useEffect, useState } from 'react';
-import SectionTitle from '../../../Components/SectionTitle/SectionTitle';
-import MenuItem from '../../Shared/MenuItem/MenuItem';
+import React, { useEffect, useState } from "react";
+import SectionTitle from "../../../Components/SectionTitle/SectionTitle";
+import MenuItem from "../../Shared/MenuItem/MenuItem";
+import { useMenu } from "../../../Hooks/useMenu/useMenu";
 
 const PopularMenu = () => {
-    const [menu,setMenu] = useState([]);
+  const [popularMenu, setpopularMenu] = useState([]);
+  const [menu] = useMenu("menu.json");
 
-    useEffect(()=>{
-        fetch('menu.json')
-        .then(res => res.json())
-        .then(data => {
-            const popularItems = data.filter(item => item.category === 'popular')
-            setMenu(popularItems)
-        })
-    },[])
-    return (
-        <div className='mt-6'>
-            <div className='mb-4'>
-            <SectionTitle heading={"From Our Menu"}
-            subHeading={"Check it out"}></SectionTitle>
-            </div>
-           <div className='grid md:grid-cols-2 gap-4 p-4'>
-           {
+  useEffect(() => {
+    const popularItems = menu?.filter((item) => item.category === "popular");
+    setpopularMenu(popularItems);
+  }, [menu]);
 
-              menu.map(item => <MenuItem  key={item._id} item={item}></MenuItem>)  
-            }
-           </div>
-        </div>
-    );
+  return (
+    <div className="mt-6">
+      <div className="mb-4">
+        <SectionTitle
+          heading={"From Our Menu"}
+          subHeading={"Check it out"}
+        ></SectionTitle>
+      </div>
+      <div className="grid md:grid-cols-2 gap-4 p-4">
+        {menu &&
+          popularMenu.map((item) => (
+            <MenuItem key={item._id} item={item}></MenuItem>
+          ))}
+      </div>
+    </div>
+  );
 };
 
 export default PopularMenu;
